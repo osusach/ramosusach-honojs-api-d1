41b1e658062fc4777f0475dcbcab84c7020b0d4a
Kappappita <ignacioladal@gmail.com>
2023-02-15 05:12:00 -0300

    feat: added tiny text to be returned when using no-subroutes

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,11 @@ export interface Env {
 }
 
 const app = new Hono<{Bindings: Env}>()
+app.get('/', async (c)=> {
+	return c.text("Holaaa, nos alegra que estés probando esta API, para obtener \
+				   la información debes ocupar las rutas /courses, /careers o /faculties. \
+				   Puedes encontrar instrucciones más detalladas en https://github.com/open-source-usach/ramosusach-honojs-api-d1")
+})
 
 app.route('/careers', careers)
 app.route('/courses', courses)
